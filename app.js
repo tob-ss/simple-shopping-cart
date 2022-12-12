7ac0cbc019a04ed44447e864c981c56be49898fb
Tobs <tobaodumosu1@gmail.com>
2022-12-12 02:56:30 +0000

    Chore: Improved responsiveness and notes in app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+/* when you click on add to cart, takes the title, price and adds to cart
+if you click on add to cart again, needs to increase quantity
+total price needs to be updated
+appropriate divs need to be added to cart
